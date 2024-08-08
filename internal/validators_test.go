26fb1fd416effef911fab80545dcef9de1deafb2
Everton Pereira <me@ton.dev.br>
2024-08-08 15:28:54 +0100

    Add license comment in all go files

--- internal/validators_test.go
+++ internal/validators_test.go
@@ -1,3 +1,8 @@
+/*
+This file is licensed under the Creative Commons Attribution-NonCommercial 4.0 International License.
+You may obtain a copy of the license at https://creativecommons.org/licenses/by-nc/4.0/legalcode.txt
+*/
+
 package internal
 
 import "testing"
